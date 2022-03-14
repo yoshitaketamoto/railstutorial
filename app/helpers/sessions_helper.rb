517f3bc9b522cd73b7d4e3ea036ba9420b76377d
yoshitaketamoto <tamoto@yamanamitech.com>
2022-03-14 06:37:11 +0000

    例外発生部分を削除

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,12 +12,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # 記憶トークンcookieに対応するユーザーを返す
+ # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      raise       # テストがパスすれば、この部分がテストされていないことがわかる
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
