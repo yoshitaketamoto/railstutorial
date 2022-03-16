3d0673ccd4ff0a45a1fb2df1b63f84c56661ad11
yoshitaketamoto <tamoto@yamanamitech.com>
2022-03-16 02:53:32 +0000

    管理者権限の制御をアクションレベルでテスト

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,24 +1,19 @@
 require 'test_helper'
 
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-  
+class UsersControllerTest < ActionDispatch::IntegrationTest
+
   def setup
     @user = users(:michael)
     @other_user = users(:archer)
   end
 
   test "should get new" do
-    get login_path
+    get signup_path
     assert_response :success
   end
-  
+
   test "should redirect index when not logged in" do
     get users_path
-    assert_redirected_to login_url
-  end
-  test "should redirect edit when not logged in" do
-    get edit_user_path(@user)
-    assert_not flash.empty?
     assert_redirected_to login_url
   end
 
@@ -28,7 +23,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-  
+
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
@@ -41,6 +36,21 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
     assert_redirected_to root_url
   end
 end
