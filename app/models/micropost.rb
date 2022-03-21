0c95f2d664dfedcf1b9a2771be66b457552d4f66
yoshitaketamoto <tamoto@yamanamitech.com>
2022-03-21 07:54:06 +0000

    表示用のリサイズ済み画像を追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,4 +8,9 @@ class Micropost < ApplicationRecord
                                       message: "must be a valid image format" },
                       size:         { less_than: 5.megabytes,
                                       message: "should be less than 5MB" }
+
+  # 表示用のリサイズ済み画像を返す
+  def display_image
+    image.variant(resize_to_limit: [500, 500])
+  end
 end
