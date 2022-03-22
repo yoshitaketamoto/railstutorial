74dfa6385f820285a0beee7e78ec67e45d787cef
yoshitaketamoto <tamoto@yamanamitech.com>
2022-03-22 02:44:55 +0000

    フィードの最終的な実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,10 +77,12 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
   
-  # ユーザーのステータスフィードを返す
+ # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
-     following_ids: following_ids, user_id: id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
   
    # ユーザーをフォローする
