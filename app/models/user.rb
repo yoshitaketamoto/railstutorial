e0f26d1c7283dd4e0befe108e4b755859ddd5306
yoshitaketamoto <tamoto@yamanamitech.com>
2022-03-22 01:47:09 +0000

    followersに対するテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
-  
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
