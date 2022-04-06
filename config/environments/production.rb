3f14b655a883a3ac454ea7be44f6992e268c2296
yoshitaketamoto <tamoto@yamanamitech.com>
2022-04-06 02:34:43 +0000

    hostの変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://frozen-bayou-97641.herokuapp.com'
+  host = 'tamoto@yamanamitech.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -76,7 +76,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'yamanamitech.com',
+    :domain         => host,
     :enable_starttls_auto => true
   }
 
