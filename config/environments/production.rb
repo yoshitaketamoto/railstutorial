fe5d07da8ac78de6c02ee2318c4aa2fbe230391f
yoshitaketamoto <tamoto@yamanamitech.com>
2022-04-06 01:52:08 +0000

    ドメインの変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => host,
+    :domain         => 'yamanamitech.com',
     :enable_starttls_auto => true
   }
 
